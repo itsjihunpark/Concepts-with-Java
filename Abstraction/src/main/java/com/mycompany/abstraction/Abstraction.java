ce6fa56999ac3d576ae5622bc4d6f1836d8f1739
Jihun Park <jihunpark0989@gmail.com>
2023-07-19 12:42:39 +0100

    Renamed folder

---
+++ Abstraction/src/main/java/com/mycompany/abstraction/Abstraction.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.abstraction;
+
+/**
+ *
+ * @author Jihun
+ */
+public class Abstraction {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
