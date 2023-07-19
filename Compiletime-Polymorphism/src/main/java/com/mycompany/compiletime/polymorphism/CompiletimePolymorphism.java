0b04682abe3ba758e50ad4bbcb2e034368180333
Jihun Park <jihunpark0989@gmail.com>
2023-07-19 12:44:09 +0100

    Added compile time polymorphism concept

---
+++ Compiletime-Polymorphism/src/main/java/com/mycompany/compiletime/polymorphism/CompiletimePolymorphism.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compiletime.polymorphism;
+
+/**
+ *
+ * @author Jihun
+ */
+public class CompiletimePolymorphism {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
