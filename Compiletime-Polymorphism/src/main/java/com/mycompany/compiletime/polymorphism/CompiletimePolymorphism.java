ee87d19c30e7262defaf7694dc640aeeb75e62fa
Jihun Park <jihunpark0989@gmail.com>
2023-07-19 14:11:54 +0100

    Added compile time polymorphism concepts

--- Compiletime-Polymorphism/src/main/java/com/mycompany/compiletime/polymorphism/CompiletimePolymorphism.java
+++ Compiletime-Polymorphism/src/main/java/com/mycompany/compiletime/polymorphism/CompiletimePolymorphism.java
@@ -4,6 +4,9 @@
 
 package com.mycompany.compiletime.polymorphism;
 
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
 /**
  *
  * @author Jihun
@@ -11,6 +14,30 @@ package com.mycompany.compiletime.polymorphism;
 public class CompiletimePolymorphism {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Predicate<Integer> isDivisibleByThree = n -> Math.abs(n) % 3 == 0;
+        ConditionArrayList divisibleByThreeListy =
+                new ConditionArrayList(isDivisibleByThree);
+
+        ConditionArrayList divisibleByThreeListy2 =
+                new ConditionArrayList(isDivisibleByThree,
+                        1, 2, 3, 4, 5, 6, 9);
+
+        ArrayList<Integer> numsList = new ArrayList<>();
+        numsList.add(1);
+        numsList.add(4);
+        numsList.add(3);
+        numsList.add(6);
+        numsList.add(13);
+
+        ConditionArrayList divisibleByThreeListy3 =
+                new ConditionArrayList(isDivisibleByThree,
+                        numsList);
+
+        Predicate<Integer> isDivisibleBySix = n-> Math.abs(n) % 6 ==0;
+        ConditionArrayList divisibleBySix = new ConditionArrayList(isDivisibleBySix, divisibleByThreeListy); 
+        //Constructor overloading, program decide which constructor to use based 
+        //on number of inputs and arguments passed at compile time. Hence, 
+        //compile time polymorphism
+
     }
 }
---
+++ Compiletime-Polymorphism/src/main/java/com/mycompany/compiletime/polymorphism/ConditionArrayList.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.compiletime.polymorphism;
+
+/**
+ *
+ * @author Jihun
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+
+public class ConditionArrayList extends ArrayList<Integer> {
+
+    private Predicate<Integer> condition;
+
+    public ConditionArrayList(Predicate<Integer> predicate,
+                              Integer... nums) {
+        super(new ConditionArrayList(predicate, Arrays.asList(nums))); //reuses below constructor
+    }
+    public ConditionArrayList(Predicate<Integer> predicate, List<Integer> list) //compile time polymorphism
+    {
+        super(list.stream().filter(predicate).collect(Collectors.toList()));
+        this.condition = predicate;
+    }
+
+    @Override
+    public void add(int index, Integer element) {
+        if (isEligible(element)) {
+            super.add(index, element);
+        }
+    }
+
+    @Override
+    public boolean add(Integer element) {
+        if (isEligible(element)) {
+            return super.add(element);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Integer> c) {
+        return super.addAll(c.stream()
+                .filter(this::isEligible)
+                .collect(Collectors.toList()));
+    }
+
+    @Override
+    public boolean addAll(int index,
+                          Collection<? extends Integer> c) {
+        return super.addAll(index, c.stream()
+                .filter(this::isEligible)
+                .collect(Collectors.toList()));
+    }
+
+    @Override
+    public Integer set(int index,
+                       Integer element) {
+        if (isEligible(element)) {
+            return super.set(index, element);
+        } else {
+            System.out.println(element + " is not odd. ");
+            return Integer.MIN_VALUE;
+        }
+    }
+
+
+    @Override
+    public void replaceAll(UnaryOperator<Integer> operator) {
+        super.replaceAll(operator);
+        super.removeIf(n -> !isEligible(n));
+    }
+
+    public boolean isEligible(Integer element) {
+        return condition.test(element);
+    }
+
+}
